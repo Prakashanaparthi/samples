0a4e25407afa993f6f7626d5fe3f1bab128f67ea
Prakashanaparthi <113918954+Prakashanaparthi@users.noreply.github.com>
2024-06-20 22:13:47 +0530

    third

--- src/Components/CartPage.js
+++ src/Components/CartPage.js
@@ -33,21 +33,39 @@ function CartPage() {
                 ) : (
                     <div>
                         <ul>
+                        <div className='column'>
+                            <h1>Quantity</h1>
+                            <h1>image</h1>
+                            <h1>Name</h1>
+                            <h1>Price</h1>
+                            <h1>Remove</h1>
+                        </div>
                             {cartItems.map((item, index) => (
                                 <li key={index}>
                                     <div className="cart-item">
                                         <div className="cart-item-details">
+                                            <div className='boxes'>
+                                                <img className='img' src={item.img}/>
+                                            </div>
+                                            <div className='boxes'>
                                             <h3 className='productNames'>{item.name}</h3>
+                                            </div>
+                                            <div className='boxes'>
                                             <p className='productNames'>Price: ${item.price}</p>
+                                            </div>
+                                            <div className='boxes'>
                                             <p className='productNames'>Quantity: {item.count}</p>
+                                            </div>
+                                            <div className='boxes'>
                                             <button onClick={() => removeFromCart(index)}>Remove</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </li>
                             ))}
                         </ul>
                         <div className="total">
-                            <h2>Total: ${calculateTotal().toFixed(2)}</h2>
+                            <h2 className='total'>Total: ${calculateTotal().toFixed(2)}</h2>
                         </div>
                     </div>
                 )}
--- src/Components/wishlistPage.js
+++ src/Components/wishlistPage.js
@@ -26,15 +26,31 @@ function WishlistPage() {
             <div className="wishlist-items">
                 {wishlistItems.length === 0 ? (
                     <p className='heading'>Your wishlist is empty.</p>
+                    
                 ) : (
                     <ul>
+                        <div className='column'>
+                            <h1>image</h1>
+                            <h1>Name</h1>
+                            <h1>Price</h1>
+                            <h1>Remove</h1>
+                        </div>
                         {wishlistItems.map((item, index) => (
                             <li key={index}>
                                 <div className="wishlist-item">
                                     <div className="wishlist-item-details">
+                                        <div className='border'>
+                                        <img className='wishlistImg' src={item.img}/>
+                                        </div>
+                                        <div className='border'>
                                         <h3 className='productNames'>{item.name}</h3>
+                                        </div>
+                                        <div className='border'>
                                         <p className='productNames'>Price: ${item.price}</p>
-                                        <button onClick={() => removeFromWishlist(index)}>Remove</button>
+                                        </div>
+                                        <div className='border'>
+                                        <button className='wishListBtn' onClick={() => removeFromWishlist(index)}>Remove</button>
+                                        </div>
                                     </div>
                                 </div>
                             </li>
